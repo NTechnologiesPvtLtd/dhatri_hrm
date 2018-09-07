dbded2a678ed9b6228c4f32667060adad0b11933
saikiran <saikiran.vtalent@gmail.com>
2018-09-07 10:48:07 +0530

    Beanclass Created

--- src/com/hrm/bean/EmployeeBean.java
+++ src/com/hrm/bean/EmployeeBean.java
@@ -8,5 +8,155 @@ package com.hrm.bean;
  *
  */
 public class EmployeeBean {
+	private String empid;
+	private String firstname;
+	private String lastname;
+	private String gender;
+	private String dob;
+	private String emailid;
+	private long mobilenumber;
+	private int aadhar;
+	private String permanentaddress;
+	private String localaddress;
+
+	private String schoolname;
+	private String intermediatecollegename;
+	private String graduationdetails;
+	private String branch;
+
+	private String companyname;
+	private String role;
+	private String noticeperiod;
+	private String location;
+	private String experience;
+	private double currentctc;
+	private double expectedctc;
+	
+	public String getEmpid() {
+		return empid;
+	}
+	public void setEmpid(String empid) {
+		this.empid = empid;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+	public long getMobilenumber() {
+		return mobilenumber;
+	}
+	public void setMobilenumber(long mobilenumber) {
+		this.mobilenumber = mobilenumber;
+	}
+	public int getAadhar() {
+		return aadhar;
+	}
+	public void setAadhar(int aadhar) {
+		this.aadhar = aadhar;
+	}
+	public String getPermanentaddress() {
+		return permanentaddress;
+	}
+	public void setPermanentaddress(String permanentaddress) {
+		this.permanentaddress = permanentaddress;
+	}
+	public String getLocaladdress() {
+		return localaddress;
+	}
+	public void setLocaladdress(String localaddress) {
+		this.localaddress = localaddress;
+	}
+	public String getSchoolname() {
+		return schoolname;
+	}
+	public void setSchoolname(String schoolname) {
+		this.schoolname = schoolname;
+	}
+	public String getIntermediatecollegename() {
+		return intermediatecollegename;
+	}
+	public void setIntermediatecollegename(String intermediatecollegename) {
+		this.intermediatecollegename = intermediatecollegename;
+	}
+	public String getGraduationdetails() {
+		return graduationdetails;
+	}
+	public void setGraduationdetails(String graduationdetails) {
+		this.graduationdetails = graduationdetails;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getCompanyname() {
+		return companyname;
+	}
+	public void setCompanyname(String companyname) {
+		this.companyname = companyname;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getNoticeperiod() {
+		return noticeperiod;
+	}
+	public void setNoticeperiod(String noticeperiod) {
+		this.noticeperiod = noticeperiod;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getExperience() {
+		return experience;
+	}
+	public void setExperience(String experience) {
+		this.experience = experience;
+	}
+	public double getCurrentctc() {
+		return currentctc;
+	}
+	public void setCurrentctc(double currentctc) {
+		this.currentctc = currentctc;
+	}
+	public double getExpectedctc() {
+		return expectedctc;
+	}
+	public void setExpectedctc(double expectedctc) {
+		this.expectedctc = expectedctc;
+	}
 
 }
