5a7a5acaf0dd4deb88ea24c7932fb087c2c6a5e4
Narendar <narendernalla478@gmail.com>
2018-09-12 12:12:40 +0530

    Us1003-HRMDaoImplementation class modified.

--- src/com/hrm/dao/HRMDaoImplementation.java
+++ src/com/hrm/dao/HRMDaoImplementation.java
@@ -18,31 +18,36 @@ public class HRMDaoImplementation implements HRMDao {
 	@Override
 	public int insertEmployeeData(EmployeeBean employeeBean) {
 		int result=0;
-		String query="insert into table values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+		String query="insert into table values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 		try
 		{
 			pstmt=con.prepareStatement(query);
 		pstmt.setString(1,EmployeeIdGenerator.generateId());
 		pstmt.setString(2,employeeBean.getFirstName());
 		pstmt.setString(3,employeeBean.getLastName());
-		pstmt.setString(4,employeeBean.getGender());
-		pstmt.setString(5,employeeBean.getDob().toString());
-		pstmt.setString(6,employeeBean.getEmailId());
-		pstmt.setLong(7,employeeBean.getMobileNumber());
-		pstmt.setLong(8,employeeBean.getAadharNo());
-		pstmt.setString(9,employeeBean.getPermanentAddress());
-		pstmt.setString(10,employeeBean.getLocalAddress());
-		pstmt.setString(11,employeeBean.getSchoolName());
-		pstmt.setString(12,employeeBean.getIntermediateCollegeName());
-		pstmt.setString(13,employeeBean.getGraduationDetails());
-		pstmt.setString(14,employeeBean.getBranch());
-		pstmt.setString(15,employeeBean.getCompanyName());
-		pstmt.setString(16,employeeBean.getRole());
-		pstmt.setInt(17,employeeBean.getNoticePeriod());
-		pstmt.setString(18,employeeBean.getLocation());
-		pstmt.setString(19,employeeBean.getExperience());
-		pstmt.setDouble(20,employeeBean.getCurrentCTC());
-		pstmt.setDouble(21,employeeBean.getExpectedCTC());
+		pstmt.setString(4,employeeBean.getFatherName());
+		pstmt.setString(5,employeeBean.getGender());
+		pstmt.setString(6,employeeBean.getDob().toString());
+		pstmt.setString(7,employeeBean.getEmailId());
+		pstmt.setLong(8,employeeBean.getMobileNumber());
+		pstmt.setLong(9,employeeBean.getAadharNo());
+		pstmt.setString(10,employeeBean.getPermanentAddress());
+		pstmt.setString(11,employeeBean.getLocalAddress());
+		pstmt.setString(12,employeeBean.getSchoolName());
+		pstmt.setInt(13, employeeBean.getTenthPassedOut());
+		pstmt.setString(14,employeeBean.getIntermediateCollegeName());
+		pstmt.setInt(15,employeeBean.getInterPassedOut());
+		pstmt.setString(16,employeeBean.getGraduationDetails());
+		pstmt.setInt(17,employeeBean.getGraduationPaseedOut());
+		pstmt.setString(18,employeeBean.getBranch());
+		pstmt.setString(19,employeeBean.getUniversity());
+		pstmt.setString(20,employeeBean.getCompanyName());
+		pstmt.setString(21,employeeBean.getRole());
+		pstmt.setInt(22,employeeBean.getNoticePeriod());
+		pstmt.setString(23,employeeBean.getLocation());
+		pstmt.setString(24,employeeBean.getExperience());
+		pstmt.setDouble(25,employeeBean.getCurrentCTC());
+		pstmt.setDouble(26,employeeBean.getExpectedCTC());
 		result=pstmt.executeUpdate();
 		
 		}
