2cb556982b3574421b55c42ad475dd1ca34d85c5
Manisankar221 <manisankar.vtalent@gmail.com>
2021-10-09 02:59:14 -0700

    @push

--- src/com/hrm/bean/DummyUtil.java
+++ src/com/hrm/bean/DummyUtil.java
@@ -5,15 +5,10 @@ public class DummyUtil {
 	public static Boolean returnBooleanValue(String val) {
 		Boolean value = null;
 		if(val.isEmpty()) {
-			
 			return value;
-<<<<<<< HEAD
 		}	
-=======
-		}
 		
->>>>>>> f79cf120dc8e774686579f2855d63f7b4e9a42e8
-		return value;
+		 return value;
 		
 	}
 
