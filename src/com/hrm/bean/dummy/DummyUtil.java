7c695babcf6f1cd77e2a972b102952d3f92a7ae2
Narsingh <narsinghraom.dhatriinfo@gmail.com>
2021-10-09 12:12:23 +0530

    testing

---
+++ src/com/hrm/bean/dummy/DummyUtil.java
@@ -0,0 +1,18 @@
+package com.hrm.bean.dummy;
+
+public class DummyUtil {
+	private DummyUtil() {
+		//hiding the implemnetation
+	}
+	public static Boolean returnBooleanValue(String val) {
+
+		Boolean value = null;
+		if (val.isEmpty()) {
+			return value;
+		}
+
+		return null;
+
+	}
+
+}
