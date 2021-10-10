bb0ca5bffc1c1132d7e536094703ed7f8472e9c9
Vamshi <dakshapu.vamshi.vtalent@gmail.com>
2021-10-10 20:26:17 +0530

    Added @Override annotation

--- src/com/hrm/controller/SearchEmployee.java
+++ src/com/hrm/controller/SearchEmployee.java
@@ -34,6 +34,7 @@ public class SearchEmployee extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
@@ -42,6 +43,7 @@ public class SearchEmployee extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+    @Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String searchparam = request.getParameter("val");
