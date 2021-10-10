5ae52e0dc976b9fdb614b4ce4dbc96b5d061ac64
AlakuntaEshwar <alakunta.eshwar.vtalent@gmail.com>
2021-10-10 11:12:11 +0530

    no need string

--- src/com/hrm/businesslogic/EmployeeIdGenerator.java
+++ src/com/hrm/businesslogic/EmployeeIdGenerator.java
@@ -10,7 +10,7 @@ public class EmployeeIdGenerator
 	
 	public static String getEmployeeId()
 	{
-		//employeeId= new String();
+		///employeeId= new String();
 		employeeId="DS-"+unqiueNo();
 		return employeeId;
 	}
