f302239e6e96d926214421af4e36e426a7f3a824
java15batch <kranthikumarnalla.vtalent@gmail.com>
2021-10-10 20:50:25 -0700

    testing

--- src/com/hrm/bean/DummyUtil.java
+++ src/com/hrm/bean/DummyUtil.java
@@ -1,20 +1,15 @@
 package com.hrm.bean;
 
 public class DummyUtil {
-	
-	public static Boolean returnBooleanValue(String val) {
-		Boolean value = null;
-		if(val.isEmpty()) {
-			
-			return value;
-<<<<<<< HEAD
-		}	
-=======
-		}
-		
->>>>>>> f79cf120dc8e774686579f2855d63f7b4e9a42e8
-		return value;
-		
+	private DummyUtil() {
+		// hidding the implementation
 	}
 
+	public static Boolean returnBooleanValue(String val) {
+		Boolean value = null;
+		if (val.isEmpty()) {
+         return value;
+		}
+		return null;
+	}
 }
