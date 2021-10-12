8dc69ab686c7f23984d24a17b9644b1e42b1dc38
Gopala Krishna <gopalakrishna.maka.vtalent@gmail.com>
2021-10-12 10:04:23 +0530

    Test

--- src/com/hrm/controller/SearchData.java
+++ src/com/hrm/controller/SearchData.java
@@ -30,7 +30,8 @@ public class SearchData extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-    @Override
+    
+    @Override 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		SharedObject.setSession(request.getSession());
