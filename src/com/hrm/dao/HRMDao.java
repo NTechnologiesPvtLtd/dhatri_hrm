b24650dd8c083927438edaa4885794a5859b826a
Suraj Kumar <surajkumar.vtalent@gmail.com>
2022-01-12 09:51:36 +0530

    Sonar info changes- HRM services implementation.

--- src/com/hrm/dao/HRMDao.java
+++ src/com/hrm/dao/HRMDao.java
@@ -11,24 +11,25 @@ public interface HRMDao
 	//abstract method
 	public abstract int insertEmployeeData(EmployeeBean employeeBean);
 	public abstract List<EmployeeBean> search();
-	public abstract List<RequestBean>ViewRequest();
+	public abstract List<RequestBean>viewRequest();
 	public abstract String getRole(String userId);
 	public abstract String getMail(String userId);
-	public abstract List<RequestBean>ViewSenderMialId(String emailID);
-	public abstract List<RequestBean>ViewReceiverMialId(String emailID);
+	public abstract List<RequestBean>viewSenderMialId(String emailID);
+	public abstract List<RequestBean>viewReceiverMialId(String emailID);
 
 	public abstract List<EmployeeBean> search(String param) throws Exception;
 	public abstract boolean login(String employeeeId, long mobileNumber);
 	public abstract  int employeeUpdate(EmployeeBean emp);
 	public List<EmployeeBean> searchManager(String role1);
-	public List<EmployeeBean> searchManagerEmp(String Manager);
+	public List<EmployeeBean> searchManagerEmp(String manager);
 	public EmployeeBean searchGmailId(String str1);
-	//public abstract int delete(EmployeeBean emp)throws Exception;
-	//int delete(String employeeId) throws Exception;
+	public abstract int delete(EmployeeBean emp)throws Exception;
+	int delete(String employeeId) throws Exception;
 	public abstract int insertRequestData(RequestBean requestBean);
 
-	/* public abstract int insertAdminData(AdminBean adminBean); */
+	public abstract int insertAdminData(AdminBean adminBean);
 	public abstract boolean adminlogin(String username, String password);
 	public abstract List<RequestBean> requestSearch();
 	public abstract List<EmployeeBean> employeeSearch(String searchparam);
+	
 }
--- src/com/hrm/services/HRMService.java
+++ src/com/hrm/services/HRMService.java
@@ -2,29 +2,31 @@ package com.hrm.services;
 
 import java.util.List;
 
+import com.hrm.bean.AdminBean;
 import com.hrm.bean.EmployeeBean;
 import com.hrm.bean.RequestBean;
 
 public interface HRMService {
 	public abstract int insertEmployeeData(EmployeeBean employeeBean);
 	public abstract List<EmployeeBean> search();
-	public abstract List<RequestBean>ViewRequest();
+	public abstract List<RequestBean>viewRequest();
 	public abstract String getRole(String userId);
 	public abstract String getMail(String userId);
-	public abstract List<RequestBean>ViewSenderMialId(String emailID);
-	public abstract List<RequestBean>ViewReceiverMialId(String emailID);
+	public abstract List<RequestBean>viewSenderMialId(String emailID);
+	public abstract List<RequestBean>viewReceiverMialId(String emailID);
 
 	public abstract List<EmployeeBean> search(String param) throws Exception;
 	public abstract boolean login(String employeeeId, long mobileNumber);
 	public abstract int employeeUpdate(EmployeeBean emp) ;
 	public List<EmployeeBean> searchManager(String role1);
 	public EmployeeBean searchGmailId(String str1);
-	public List<EmployeeBean> searchManagerEmp(String Manager);
+	public List<EmployeeBean> searchManagerEmp(String manager);
 	public abstract int insertRequestData(RequestBean requestBean);
 
-	/* public abstract int insertAdminData(AdminBean adminBean); */
+	public abstract int insertAdminData(AdminBean adminBean);
 	public abstract boolean adminlogin(String adminid,String password);
 	public abstract List<RequestBean> requestSearch();
 	public abstract List<EmployeeBean> employeeSearch(String searchparam);
+	
 
 }
